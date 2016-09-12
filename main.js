ee378310e8ba192f6a22a5f6b0bb499d892b0fe2
underOneFlag <underOneFlag@users.noreply.github.com>
2016-09-11 22:35:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const _uOF = {};
 
 _uOF.bgColorChangers = Array.from(document.querySelectorAll('body, button'));
 _uOF.colorChanger = document.querySelector('section');
-_uOF.quoteElement = document.querySelector('blockquote');
+_uOF.fillChanger = document.querySelector('svg');
+_uOF.quoteElement = document.querySelector('section > p');
 _uOF.citationElement = document.querySelector('citation');
 _uOF.quote = undefined;
 _uOF.citation = undefined;
@@ -15,6 +16,7 @@ _uOF.quotes = [
 
 _uOF.colorSwitcher = (color) => {
 	_uOF.bgColorChangers.forEach(x => x.style.backgroundColor = color);
+	_uOF.fillChanger.style.fill = color;
 	_uOF.colorChanger.style.color = color;
 };
 
@@ -26,27 +28,41 @@ _uOF.colorChooser = () => {
 	return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
 };
 
-_uOF.getQuote = () => {
-	return _uOF.quotes[Math.floor(Math.random() * 3)];
-	return ['test', 'test2'];
+// _uOF.getQuote = () => {
+// 	return _uOF.quotes[Math.floor(Math.random() * 3)];
+// 	return ['test', 'test2'];
+// };
+
+_uOF.getQuote = (cb) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => cb(null, xhr.responseText);
+  xhr.onerror = (err) => cb(err);
+  xhr.open('POST', 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous');
+  xhr.setRequestHeader("X-Mashape-Key", "grpIxjBKFbmshGIOweKMjBkYsGUup1l7HQIjsnGIQUHDcW6GPH")
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.send();
 };
 
-_uOF.insertQuote = (quote, citation) => {
-	if(quote === undefined || citation === undefined)
+_uOF.insertQuote = (err, data) => {
+	if(err) {
+		alert('Error retrieving quote.');
 		return;
+	}
 	
-	_uOF.quote = quote;
-	_uOF.citation = citation;
+	data = JSON.parse(data);
+	
+	_uOF.quote = data.quote;
+	_uOF.citation = data.author;
 
-	_uOF.quoteElement.textContent = quote;
-	_uOF.citationElement.textContent = citation;
+	_uOF.quoteElement.textContent = data.quote;
+	_uOF.citationElement.textContent = data.author;
 };
 
 _uOF.newQBtn = document.querySelector('button:last-of-type');
 _uOF.newQBtn.onclick = (e) => {
 	_uOF.colorSwitcher(_uOF.colorChooser());
-	_uOF.insertQuote(..._uOF.getQuote());
-	return true;;
+	_uOF.getQuote(_uOF.insertQuote);
 };
 
 _uOF.newQBtn.click();
